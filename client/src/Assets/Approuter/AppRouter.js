ca026c09a136b57d857495602cd110bc4c5624cb
Oriz1206 <phamtrungnguyen1206@gmail.com>
2024-05-16 11:09:21 +0700

    navigate route
     mặc định mới vào sẽ dẫn đến login

--- client/src/Assets/Approuter/AppRouter.js
+++ client/src/Assets/Approuter/AppRouter.js
@@ -1,6 +1,7 @@
 // src/Assets/Approute/AppRouter.js
 import React from 'react';
-import {Routes, Route, Outlet, useNavigate, json,} from "react-router-dom";import Login from '../Page/UserAuth/Login';
+import {Routes, Route,  Outlet, useNavigate, json, Navigate } from "react-router-dom";
+import Login from '../Page/UserAuth/Login';
 import Signup from '../Page/UserAuth/Signup';
 import { RoleProvider } from '../Page/UserAuth/Role';
 
@@ -8,14 +9,29 @@ import { RoleProvider } from '../Page/UserAuth/Role';
 function AppRouter () {
   return (
     <RoleProvider>
-        <Routes>
-            <Route exact path="/login" element={<Login/>}></Route>
-            <Route exact path="/signup" element={<Signup/>}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route exact path="/login" element={<Login/>}></Route>
+        <Route exact path="/signup" element={<Signup/>}></Route>
+        {/* <PrivateRoute path="/recruiter/*" element={<RecruiterRoutes />} />
+        <PrivateRoute path="/seeker/*" element={<SeekerRoutes />} /> */}
+      </Routes>
     </RoleProvider>
 
     
   );
+}
+
+function PrivateRoute() {
+
+}
+
+function RecruiterRoutes() {
+
+}
+
+function SeekerRoutes() {
+
 }
 
 export default AppRouter;
